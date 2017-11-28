1986ed269d61991b84165d39ef000c08144b9911
Troy Amelotte <troy.amelotte@gTAef49b1.local>
2017-11-28 12:54:45 -0700

    added timer

--- RewindBehavior.cs
+++ RewindBehavior.cs
@@ -6,40 +6,37 @@ public class RewindBehavior : MonoBehaviour {
 
 	private List<Vector2> positionList = new List<Vector2>();
 	private GameObject Player;
-	public Rigidbody2D rb;
+	Rigidbody2D rb;
+	public GameObject Timer;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("LogProgress", 0.1f, 0.1f);
 		Player = this.gameObject;
 		rb = GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
-	void Update(){
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			CancelInvoke ();
-			rb.isKinematic = true;
-			InvokeRepeating ("RewindTime", 0.01f, 0.01f);
-		}
-	}
 	void LogProgress(){
 
 		positionList.Add (Player.transform.position);
-
-		if (positionList.Count > 50) {
-			positionList.RemoveAt (0);
-		}
 		Debug.Log (positionList[positionList.Count-1]);
 	}
-	
+
+	void StartRewind(){
+		CancelInvoke ();
+		rb.isKinematic = true;
+		InvokeRepeating ("RewindTime", 0.02f, 0.02f);
+	}
+
 	void RewindTime(){
 		if (positionList.Count > 0) {
 			Player.transform.position = positionList [positionList.Count - 1];
+			rb.velocity = new Vector2 (0, 0);
 			positionList.RemoveAt (positionList.Count - 1);
 		} else {
 			CancelInvoke ();
+			Timer.SendMessage ("RestartTime");
 			rb.isKinematic = false;
-			InvokeRepeating ("LogProgress", 0.5f, 0.5f);
+			positionList.Clear ();
+			InvokeRepeating ("LogProgress", 0.25f, 0.25f);
 		}
 			
 	}
---
+++ Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	// Use this for initialization
+	Text txt;
+	public GameObject player;
+	void Start () {
+		txt = this.GetComponent<Text>();
+		timeLeft = rewindTime;
+	}
+	
+	public float rewindTime = 8.0f;
+	float timeLeft;
+	bool isRunning = true;
+	void RestartTime()
+	{
+		timeLeft = rewindTime;
+		txt.color = Color.black;
+		isRunning = true;
+	}
+
+	void Update()
+	{
+		if (isRunning) {
+			
+			timeLeft -= Time.deltaTime;
+			txt.text = timeLeft.ToString ("0.00");
+			if (timeLeft < 0) {
+				timeLeft = 0;
+				txt.text = timeLeft.ToString ("0.00");
+				txt.color = Color.red;
+				isRunning = false;
+				player.SendMessage ("StartRewind");
+			}
+		}
+	}
+
+}
